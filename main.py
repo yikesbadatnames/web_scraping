63776e42244f00214b216e7d3e31cdcade327433
yikesbadatnames <88944737+yikesbadatnames@users.noreply.github.com>
2022-06-11 18:38:49 -0700

    reached jobs page for remote data analyst

--- main.py
+++ main.py
@@ -18,14 +18,29 @@ chrome_options.add_experimental_option("detach", True)
 print(chromedriver_location)
 
 
-driver = webdriver.Chrome(chrome_options = chrome_options, service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(chrome_options = chrome_options, 
+                          service=Service(ChromeDriverManager().install()))
 
 driver.get('https://linkedin.com/uas/login')
 
 #waiting for the page to load
 time.sleep(5)
 
-print(user_name)
+
+username_input = driver.find_element_by_id('username')
+username_input.send_keys(user_name)
+
+password_input = driver.find_element_by_id('password')
+password_input.send_keys(password)
+
+#click sumbit
+driver.find_element_by_xpath('//*[@id="organic-div"]/form/div[3]/button').click()
 
 
+#confirm email 
+# driver.find_element_by_xpath('//*[@id="ember26"]/button[1]').click()
 
+#jobs URL
+jobs_url = 'https://www.linkedin.com/jobs/search/?distance=25.0&f_WT=2&geoId=103644278&keywords=data%20analyst'
+
+driver.get(jobs_url)
